09adaffcec6c513bcdc98c38723e26a4980e3e62
Wira Atmaja <atmajawira1908@gmail.com>
2026-06-05 23:15:52 +0800

    add data

--- resources/views/features/dashboard/destinasi-dashboard.blade.php
+++ resources/views/features/dashboard/destinasi-dashboard.blade.php
@@ -32,7 +32,7 @@
                 </p>
             </div>
 
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
                 @foreach ($topDestinations as $top)
                 {{-- <a href="{{ route('destinations.show', $top->id) }}" class="no-underline text-gray-900 block bg-white rounded-[28px] overflow-hidden hover:shadow-xl transition duration-300"> --}}
                 <a href="" class="no-underline text-gray-900 block bg-white rounded-[28px] overflow-hidden hover:shadow-xl transition duration-300">
