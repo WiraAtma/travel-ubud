b06fad83d39941f2281ff4fe2204d6974cf63f2a
Wira Atmaja <atmajawira1908@gmail.com>
2026-06-23 10:25:40 +0800

    menambahkan icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'BALI TRAVEL') }}</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('icon.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/layouts/sidebar-app.blade.php
+++ resources/views/layouts/sidebar-app.blade.php
@@ -7,6 +7,7 @@
         <title>{{ config('app.name', 'BALI TRAVEL') }}</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/x-icon" href="{{ asset('icon.png') }}">
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @vite(['resources/js/admin-dashboard.js'])
